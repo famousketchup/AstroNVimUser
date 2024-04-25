41eee777636e222face121a45ab5b1f0a330cc51
Dmitriy Korotayev <korotayev.dmitriy@gmail.com>
2024-04-25 12:44:21 +0300

    Add `nvim-dap` config for `php` adapter to listen for Xdebug via built-in debugger

---
+++ plugins/php-debug-adapter.lua
@@ -0,0 +1,21 @@
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require "dap"
+
+    dap.adapters.php = {
+      type = 'executable',
+      command = 'node',
+      args = { '~/.local/share/nvim/mason/packages/php-debug-adapter/extension/out/phpDebug.js' }
+    }
+
+    dap.configurations.php = {
+      {
+        type = 'php',
+        request = 'launch',
+        name = 'Listen for Xdebug',
+        port = 9003
+      }
+    }
+  end
+}
